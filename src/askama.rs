bd3f8d98187de5d35a5e4e36220f47d39dda16e2
valkyrie_pilot <valk@randomairborne.dev>
2024-04-10 15:01:06 -0600

    Fix path argument

--- src/askama.rs
+++ src/askama.rs
@@ -3,5 +3,5 @@ use crate::BustDir;
 /// This filter takes one argument of type `BustDir`.
 pub fn bust_dir<T: std::fmt::Display>(path: T, bd: &BustDir) -> askama::Result<String> {
     let path = path.to_string();
-    Ok(bd.get_or_random(path).to_hex().to_string())
+    Ok(bd.get_or_random(&path).to_hex().to_string())
 }
