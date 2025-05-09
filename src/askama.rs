573225f37e64adffd50aecb8ce24901db75e8f14
valkyrie_pilot <valk@randomairborne.dev>
2025-05-09 10:44:15 -0600

    Fix askama 0.14 support

--- src/askama.rs
+++ src/askama.rs
@@ -1,7 +1,11 @@
 use crate::BustDir;
 
 /// This filter takes one argument of type `BustDir`.
-pub fn bust_dir<T: std::fmt::Display>(path: T, bd: &BustDir) -> askama::Result<String> {
+pub fn bust_dir<T: std::fmt::Display>(
+    path: T,
+    _: &dyn askama::Values,
+    bustdir: &BustDir,
+) -> askama::Result<String> {
     let path = path.to_string();
-    Ok(bd.get_or_random(&path).to_hex().to_string())
+    Ok(bustdir.get_or_random(&path).to_hex().to_string())
 }
